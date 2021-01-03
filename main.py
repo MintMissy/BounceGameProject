e4f3e460ef3c39fc539a7fffc53fc8a0f7d66eab
Dawid <dawidkostka480@gmail.com>
2021-01-03 14:35:30 +0100

    After player click space ball make a smooth jump

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import ScreenProperties
 pygame.init()
 
 # Initiation of the window
-pygame.display.set_caption("The name of window")
-# TODO Setting an icon of program
-# pygame.display.set_icon()
+pygame.display.set_caption("My game in Py game")
+# Setting an icon of program
+programIcon = pygame.image.load('Graphics/ProgramIcon.png')
+pygame.display.set_icon(programIcon)
 # Import screen properties class from file
 screenProperties = ScreenProperties.ScreenProperties()
 # Set screen size
@@ -28,26 +29,22 @@ entity.setPositionY(screenProperties.getCenterY())
 entity.setSpeed(0.4)
 
 # Setting gravity speed
-entity.setGravity(0.5)
+entity.setGravity(0.2)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
-
-    screen.fill((screenProperties.getColor()))
-
-    # entity = pygame.image.load('Zadanie2_ball.png')
-    pygame.draw.circle(
-        screen,
-        entity.entityColor,
-        (entity.positionX, entity.positionY),
-        entity.radius)
+        # Check if player use space to jump ball
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                entity.setGravity(-0.6)
 
     # Gravity
     if entity.positionY + entity.radius < screenProperties.height:
         entity.setPositionY(entity.positionY + entity.gravitySpeed)
+        entity.setGravity(entity.gravitySpeed + 0.0015)
 
     # Bouncing
     # If ball don't touch bottom continue
@@ -58,12 +55,20 @@ while True:
                 entity.setBounce(False)
             # Move ball it to right
             entity.setPositionX(entity.positionX + entity.speed)
-
         else:
             # If ball touch left side change bounce direction
             if entity.positionX <= 0 + entity.radius:
                 entity.setBounce(True)
             # Move ball it to left
             entity.setPositionX(entity.positionX - entity.speed)
+
+    screen.fill((screenProperties.getColor()))
+
+    # entity = pygame.image.load('Zadanie2_ball.png')
+    pygame.draw.circle(
+        screen,
+        entity.entityColor,
+        (entity.positionX, entity.positionY),
+        entity.radius)
 
     pygame.display.flip()
